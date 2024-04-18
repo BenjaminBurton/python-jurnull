707569b7b7fe4ed7395e166e7b9582395294d836
Mr. Burton <benjaminlburton@gmail.com>
2024-04-17 22:03:53 -0400

    add stacks

---
+++ advanced/stacks.py
@@ -0,0 +1,16 @@
+# Advanced Data Structures: Advanced Python
+
+# Stacks:
+
+# A stack is a collection of elements that follows the Last In, First Out (LIFO) principle. You can use lists in Python to implement a stack.
+
+# Example: Stack Implementation using Lists
+stack = []
+
+# Push elements onto the stack
+stack.append(1)
+stack.append(2)
+stack.append(3)
+
+# Pop elements from the stack
+print(stack.pop()) # Output: 3
