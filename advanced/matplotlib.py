cdbe1f37e0276e1fbf3f592c9c85056c902c15a7
Mr. Burton <benjaminlburton@gmail.com>
2024-04-17 22:20:18 -0400

    add Matplotlib

---
+++ advanced/matplotlib.py
@@ -0,0 +1,15 @@
+# Data Science and Machine Learning: Advanced Python
+
+# Matplotlib:
+
+# Matplotlib is a plotting library for creating static, interactive, and animated visualizations in Python. It provides a wide variety of plots, charts, and graphs for data visualization.
+
+# Example: Using Matplotlib
+import matplotlib.pyplot as plt
+
+# Plot a simple line graph
+plt.plot([1, 2, 3, 4], [1, 4, 9, 16])
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.title('Simple Line Graph')
+plt.show()
